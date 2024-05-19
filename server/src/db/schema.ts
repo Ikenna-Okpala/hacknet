bb78dd8885511e16013775bb6d7f48a6df1dcf06
Ikenna Okpala <ikennajesse77@gmail.com>
2024-05-18 20:25:47 -0700

    Pending auth

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,6 +1,16 @@
+import { date, timestamp, varchar } from "drizzle-orm/pg-core";
 import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-  name: text("name"),
+  username: varchar("username", { length: 30 }).primaryKey(),
+  memberSince: timestamp("memberSince", { mode: "date" }),
+  badges: varchar("badges", { length: 1000 }),
+  pastHackathons: varchar("past_hackathons"),
+  favoriteProject: text("favorite_project"),
+  status: text("status", {
+    enum: ["student", "developer", "looking for work"],
+  }),
+
+  interests: text("interests"),
+  mediaLinks: text("media_links"),
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,28 @@
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 
+import cors from "cors";
+
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route";
+
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 const port = process.env.PORT || 8000;
 
 app.get("/healthcheck", (req: Request, res: Response) => {
   res.send("Works.....");
 });
+
+app.use("/api/users", userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);

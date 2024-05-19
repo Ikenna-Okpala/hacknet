d2a51be66bb972e203fbc15345007fd3f2614cdf
ZachSyv <zachary@syvenky.com>
2024-05-18 20:49:14 -0700

    fixed image and gif paths

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -14,12 +14,12 @@ export default function Home() {
         <main className="min-h-screen bg-white flex flex-col items-center justify-center relative">
           <nav className="absolute top-4 left-4">
             <button className="p-0 border-none bg-none">
-              <Image src="/profile.png" alt="Profile" width={64} height={64} />
+              <Image src="/images/profile_icon/Profile1.png" alt="Profile" width={64} height={64} />
             </button>
           </nav>
           <nav className="absolute top-4 right-4">
             <button className="p-0 border-none bg-none">
-              <Image src="/connections.png" alt="Connections" width={64} height={64} />
+              <Image src="/images/buttons/connections.png" alt="Connections" width={64} height={64} />
             </button>
           </nav>
           <nav className="absolute top-1/2 right-4 transform -translate-y-1/2 flex flex-col items-end space-y-2">
@@ -28,26 +28,26 @@ export default function Home() {
             <div className="text-black">y</div>
             <div className="text-black">z</div>
             <button className="p-0 border-none bg-none">
-              <Image src="/invite.png" alt="Invite" width={64} height={64} />
+              <Image src="/images/buttons/invite.png" alt="Invite" width={64} height={64} />
             </button>
             <button className="p-0 border-none bg-none">
-              <Image src="/disband.png" alt="Disband" width={64} height={64} />
+              <Image src="/images/buttons/disband.png" alt="Disband" width={64} height={64} />
             </button>
           </nav>
           <nav className="flex flex-col items-center space-y-8">
-            <Image src="/animation.gif" alt="Fun background animation" width={400} height={300} />
+            <Image src="/animations/SquirrelAnimation.gif" alt="Fun background animation" width={400} height={300} />
             <button className="p-0 border-none bg-none">
-              <Image src="/join_hackathon.png" alt="Join a Hackathon" width={192} height={64} />
+              <Image src="/images/buttons/joinahackathon.png" alt="Join a Hackathon" width={192} height={64} />
             </button>
           </nav>
           <nav className="absolute bottom-4 left-4">
             <button className="p-0 border-none bg-none">
-              <Image src="/judge_hackathon.png" alt="Judge A Hackathon" width={192} height={64} />
+              <Image src="/images/buttons/judgeahackathon.png" alt="Judge A Hackathon" width={192} height={64} />
             </button>
           </nav>
           <nav className="absolute bottom-4 right-4">
             <button className="p-0 border-none bg-none">
-              <Image src="/settings.png" alt="Settings" width={192} height={64} />
+              <Image src="/images/buttons/setting.png" alt="Settings" width={192} height={64} />
             </button>
           </nav>
         </main>
